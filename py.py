bcb33359821e351e7254e71d8bb4996c4d52b503
Luis <luis.lima@technolog.com.br>
2024-02-16 09:02:36 -0300

    adicionando prints

---
+++ py.py
@@ -0,0 +1,13 @@
+numero = 9
+
+print(numero > 3)
+# o conteúdo da variável numero (9) é maior que 3?
+
+print(numero < 10)
+# 9 é menor que 10?
+
+print(numero == 11)
+# 9 é igual a 11?
+
+print(numero >= 12)
+# 9 é igual ou maior que 12?
